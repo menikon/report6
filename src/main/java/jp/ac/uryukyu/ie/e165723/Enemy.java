ba95662caa1c9b7defd37772deb09b18cf8e08cd
Takahiro Sakamoto <e165723@ie.u-ryukyu.ac.jp>
2017-01-13 14:43:35 +0900

    report6の課題１の状態

---
+++ src/main/java/jp/ac/uryukyu/ie/e165723/Enemy.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e165723;
+
+/**
+ * Created by e165723 on 2016/11/26.
+ */
+public class Enemy  extends LivingThing{
+    Enemy(String name,int hitPoint,int attack){
+        super(name, hitPoint,attack);
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e165723/Hero.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165723;
+
+/**
+ * Created by e165723 on 2016/11/26.
+ */
+public class Hero extends LivingThing {
+    Hero(String name,int hitPoint,int attack){
+        super(name, hitPoint,attack);
+    }
+
+}
